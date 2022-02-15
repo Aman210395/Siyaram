466194ec1788cbc9ee89d2cda1ab56d01a6ba182
jamesjoel <jamesjoelj@gmail.com>
2022-02-15 18:53:44 +0530

    javascript day 3

---
+++ php/footer.php
@@ -0,0 +1,5 @@
+<div class="container-fluid bg-dark">
+            <p class="text-light m-0 p-4 text-center">This is Footer</p>
+        </div>
+    </body>
+</html>
---
+++ php/home.php
@@ -0,0 +1,13 @@
+<?php
+include("header.php");
+include("navbar.php");
+?>
+        <div class="container" style="min-height: 600px;">
+
+        </div>
+
+<?php
+include("footer.php");
+?>
+
+        
