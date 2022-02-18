580a20da56f744986ff84237ac2f6ef8df56fbe8
jamesjoel <jamesjoelj@gmail.com>
2022-02-18 17:47:22 +0530

    validation in jquery

---
+++ project/js/signup_val.js
@@ -0,0 +1,156 @@
+$(document).ready(function(){
+    $("#signup_submit").click(function(){
+        
+        var name = $("[name='fullname']").val();
+        var email = $("[name='email']").val();
+        var pass = $("[name='pass']").val();
+        var re_pass = $("[name='re_pass']").val();
+        var city = $("[name='city']").val();
+        var add = $("[name='add']").val();
+        var contact = $("[name='contact']").val();
+
+
+        var male = $("#male").is(":checked");
+        var female = $("#female").is(":checked");
+
+        var check = true;
+
+        if(name=="")
+        {
+            check = false;
+            $("#fullname_msg").html("Insert Your Name");
+            $("[name='fullname']").addClass("is-invalid");
+        }
+        else{
+            
+            $("#fullname_msg").html("");
+            $("[name='fullname']").removeClass("is-invalid");
+        }
+
+        if(email=="")
+        {
+            check = false;
+            $("#email_msg").html("Insert Your Email");
+            $("[name='email']").addClass("is-invalid");
+        }
+        else{
+            
+            $("#email_msg").html("");
+            $("[name='email']").removeClass("is-invalid");
+
+            var reg = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+            if(reg.test(email)==false)
+            {
+                check = false;
+                $("#email_msg").html("Email id is invalid");
+                $("[name='email']").addClass("is-invalid");
+            }
+            else{
+                $("#email_msg").html("");
+                $("[name='email']").removeClass("is-invalid");
+            }
+
+        }
+        if(pass=="")
+        {
+            check = false;
+            $("#pass_msg").html("Insert Your Password");
+            $("[name='pass']").addClass("is-invalid");
+        }
+        else{
+            
+            $("#pass_msg").html("");
+            $("[name='pass']").removeClass("is-invalid");
+        }
+        if(re_pass=="")
+        {
+            check = false;
+            $("#re_pass_msg").html("Insert Your Re-Password");
+            $("[name='re_pass']").addClass("is-invalid");
+        }
+        else{
+            
+            $("#re_pass_msg").html("");
+            $("[name='re_pass']").removeClass("is-invalid");
+
+
+            if(pass != re_pass)
+            {
+                check = false;
+                $("#re_pass_msg").html("Password and Re-Password is not matched");
+                $("[name='re_pass']").addClass("is-invalid");
+            }
+            else{
+                $("#re_pass_msg").html("");
+                $("[name='re_pass']").removeClass("is-invalid");
+            }
+
+        }
+        if(add=="")
+        {
+            check = false;
+            $("#add_msg").html("Insert Your Address");
+            $("[name='add']").addClass("is-invalid");
+        }
+        else{
+            
+            $("#add_msg").html("");
+            $("[name='add']").removeClass("is-invalid");
+        }
+        if(city=="")
+        {
+            check = false;
+            $("#city_msg").html("Select Your City");
+            $("[name='city']").addClass("is-invalid");
+        }
+        else{
+            
+            $("#city_msg").html("");
+            $("[name='city']").removeClass("is-invalid");
+        }
+        if(contact=="")
+        {
+            check = false;
+            $("#contact_msg").html("Insert Your Contact");
+            $("[name='contact']").addClass("is-invalid");
+        }
+        else{
+            
+            $("#contact_msg").html("");
+            $("[name='contact']").removeClass("is-invalid");
+
+            if(isNaN(contact))
+            {
+                check = false;
+                $("#contact_msg").html("Contact Number must be Numeric");
+                $("[name='contact']").addClass("is-invalid");
+            }
+            else{
+                $("#contact_msg").html("");
+                $("[name='contact']").removeClass("is-invalid");
+
+                if(contact.length != 10)
+                {
+                    check = false;
+                    $("#contact_msg").html("Contact Number in 10 Digit");
+                    $("[name='contact']").addClass("is-invalid");
+                }else{
+                    $("#contact_msg").html("");
+                    $("[name='contact']").removeClass("is-invalid");
+                }
+            }
+        }
+
+        if(male == false && female == false)
+        {
+            check = false;
+            $("#gender_msg").html("Select Your Gender");
+        }
+        else{
+            $("#gender_msg").html("");
+
+        }
+
+        return check;
+    })
+})
--- project/signup.php
+++ project/signup.php
@@ -15,47 +15,62 @@ include("includes/header.php");
                     <div class="form-group">
                         <label for="">Full Name</label>
                         <input type="text" name="fullname" class="form-control" />
+                        <small class="text-danger" id="fullname_msg"></small>
                     </div>
                     <div class="form-group">
                         <label for="">Email/Username</label>
                         <input type="text" name="email" class="form-control" />
+                        <small class="text-danger" id="email_msg"></small>
                     </div>
                     <div class="form-group">
                         <label for="">Password</label>
                         <input type="password" name="pass" class="form-control" />
+                        <small class="text-danger" id="pass_msg"></small>
                     </div>
                     <div class="form-group">
                         <label for="">Re-Password</label>
                         <input type="password" name="re_pass" class="form-control" />
+                        <small class="text-danger" id="re_pass_msg"></small>
                     </div>
                     <div class="form-group">
                         <label for="">Gender</label>
                         <Br />
-                        Male : <input type="radio" value="male"  name="gender"/>
-                        Female : <input type="radio" value="female"  name="gender"/>
+                        Male : <input type="radio" value="male" id="male"  name="gender"/>
+                        Female : <input type="radio" value="female" id="female" name="gender"/>
+                        <br />
+                        <small class="text-danger" id="gender_msg"></small>
                     </div>
                     <div class="form-group">
                         <label for="">Address</label>
                         <textarea class="form-control" name="add"></textarea>
+                        <small class="text-danger" id="add_msg"></small>
                     </div>
                     <div class="form-group">
                         <label for="">City</label>
                         <Br />
                         <Br />
-                        <select class="form-control" name="city">
-                            <option>Select</option>
-                            <option>Indore</option>
-                            <option>Mumbai</option>
-                            <option>Pune</option>
+                        <select  class="form-control" name="city">
+                            <option value="">Select</option>
+                            <option value="indore">Indore</option>
+                            <option value="mumbai">Mumbai</option>
+                            <option value="pune">Pune</option>
                         </select>
+                        <small class="text-danger" id="city_msg"></small>
                     </div>
+                    
                     <div class="form-group">
                         <label for="">Contact</label>
-                        <input type="text" name="contact" class="form-control" />
+                        <div class="input-group">
+                            <Div class="input-group-prepend">
+                                <span class="input-group-text">+91</span>
+                            </Div>
+                            <input type="text" name="contact" class="form-control" />
+                        </div>
+                        <small class="text-danger" id="contact_msg"></small>
                     </div>
                 </div>
                 <div class="card-footer">
-                    <input type="submit" value="Signup" class="btn btn-primary" />
+                    <input id="signup_submit" type="submit" value="Signup" class="btn btn-primary" />
                 </div>
             </div>
             </form>
@@ -66,3 +81,4 @@ include("includes/header.php");
 <?php
 include("includes/footer.php");
 ?>
+<script src="js/signup_val.js"></script>
